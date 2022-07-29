59e3135300eb0129942fd0a5f421104c2ec05fd8
Rolewen <migodins322@gmail.com>
2022-07-29 18:04:31 +0800

    feat: add description section on each day work

--- src/share-styled/content.ts
+++ src/share-styled/content.ts
@@ -5,4 +5,13 @@ export const Content = styled.div`
 	width: 360px;
 	margin: 0 auto;
 	border-radius: 8px;
+	box-shadow: 3px 3px 5px rgb(0 0 0 / 30%);
+
+	> .desc {
+		h3 {
+			text-align: left;
+			margin-top: 1.5rem;
+			font-size: 16px;
+		}
+	}
 `;
--- src/share-styled/wrapper.ts
+++ src/share-styled/wrapper.ts
@@ -4,6 +4,7 @@ export const Wrapper = styled.div`
 	min-width: 50%;
 	text-align: center;
 	margin-top: 3rem;
+	margin-bottom: 5rem;
 
 	@media (max-width: 768px) {
 		min-width: 100%;
