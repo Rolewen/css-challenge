f3ae29191c75559d26a31640f076d1f854950aa2
Rolewen <migodins322@gmail.com>
2022-07-29 10:20:24 +0800

    feat: done day2

--- src/components/day02.tsx
+++ src/components/day02.tsx
@@ -2,10 +2,88 @@ import { useState } from "react";
 import styled, { keyframes } from "styled-components";
 import { Wrapper, Content as SharedContent } from "../share-styled";
 
+/**
+ * keyframes,
+ * translate3d,
+ * cubic-bezier
+ */
+
 const line1 = keyframes`
- 0% { transform: translate3d(0, 0, 0) rotate(0deg); }
- 50% {  transform: translate3d(0, 22px, 0) rotate(0); }
- 100% { height: 100px; width: 100px; opacity: 0.6; }
+	0% { 
+		transform: translate3d(0, 0, 0) rotate(0deg);
+	}
+ 	
+	50% {
+		transform: translate3d(0, 22px, 0) rotate(0);
+	}
+
+ 	100% {
+		transform: translate3d(0, 22px, 0) rotate(45deg);
+	}
+`;
+
+const line2 = keyframes`
+	0% {
+		transform: scale(1);
+		opacity: 1;
+	}
+	100% {
+		transform: scale(0);
+		opacity: 0;
+	}
+`;
+
+const line3 = keyframes`
+	0% { 
+		transform: translate3d(0, 0, 0) rotate(0deg);
+	}
+ 	
+	50% {
+		transform: translate3d(0, -22px, 0) rotate(0);
+	}
+
+ 	100% {
+		transform: translate3d(0, -22px, 0) rotate(135deg);
+	}
+`;
+
+const line1Rev = keyframes`
+	0% {
+		transform: translate3d(0, 22px, 0) rotate(45deg);
+	}
+
+	50%{
+		transform: translate3d(0, 22px, 0) rotate(0deg);
+	}
+
+	100% {
+		transform: translate(0, 0, 0) rotate(0);
+	}
+`;
+
+const line2Rev = keyframes`
+	0% {
+		transform: scale(0);
+		opacity: 0;
+	}
+	100% {
+		transform: scale(1);
+		opacity: 1;
+	}
+`;
+
+const line3Rev = keyframes`
+	0% {
+		transform: translate3d(0, -22px, 0) rotate(135deg);
+	}
+
+	50%{
+		transform: translate3d(0, -22px, 0) rotate(0deg);
+	}
+
+	100% {
+		transform: translate(0, 0, 0) rotate(0);
+	}
 `;
 
 const Content = styled(SharedContent)`
@@ -30,13 +108,30 @@ const Content = styled(SharedContent)`
 			box-shadow: 0 2px 10px 0 rgb(0 0 0 /30%);
 		}
 
+		> :first-child {
+			animation: ${line1Rev} 0.7s cubic-bezier(0.3, 1, 0.7, 1) forwards;
+		}
+
 		> :nth-child(2) {
 			margin: 14px auto;
+			animation: ${line2Rev} 0.7s cubic-bezier(0.3, 1, 0.7, 1) forwards;
+		}
+
+		> :last-child {
+			animation: ${line3Rev} 0.7s cubic-bezier(0.3, 1, 0.7, 1) forwards;
 		}
 
 		&.active {
 			> :first-child {
-				animation: ${line1} cubic-bezier(0.3, 1, 0.7, 1) forwards;
+				animation: ${line1} 0.7s cubic-bezier(0.3, 1, 0.7, 1) forwards;
+			}
+
+			> :nth-child(2) {
+				animation: ${line2} 0.7s cubic-bezier(0.3, 1, 0.7, 1) forwards;
+			}
+
+			> :last-child {
+				animation: ${line3} 0.7s cubic-bezier(0.3, 1, 0.7, 1) forwards;
 			}
 		}
 	}
